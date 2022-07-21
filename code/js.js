e20d22a2e057a9036315344935d335da49c85d46
jgbae97 <62528763+jgbae97@users.noreply.github.com>
2022-07-21 16:19:59 +0900

    edit

---
+++ code/js.js
@@ -0,0 +1,13 @@
+//Dom
+const playground = document.querySelector(".playground > ul");
+
+for(let i=0; i<20; i++){
+	const li = document.createElement("li");
+	const ul = document.createElement("ul");
+	for(let j=0; j<10; j++){
+		const matrix = document.createElement("li");
+		ul.prepend(matrix);
+	}
+	li.prepend(ul);
+	playground.prepend(li)
+}
